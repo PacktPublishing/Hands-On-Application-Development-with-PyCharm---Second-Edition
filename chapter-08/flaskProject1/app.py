9b8908f30ab003907a945f785a51d2dc6496b9d2
Bruce Van Horn <actual@contentfour.com>
2023-06-12 21:54:04 -0500

    finishin up flask project

--- chapter-08/flaskProject1/app.py
+++ chapter-08/flaskProject1/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
+library_data = list()
+
+library_data.append({"python_library": "Flask",
+                     "description": "An unopinionated web framework",
+                     "rating": 5,
+                     "url": "https://pypi.org/project/Flask"})
+library_data.append({"python_library": "Jinja2",
+                     "description": "Templating library",
+                     "rating": 3,
+                     "url": "https://pypi.org/project/Jinja2"})
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def root():  # put application's code here
-    return render_template("index.html")
+    return render_template("index.html", library_data=library_data)
 
 
 if __name__ == '__main__':
