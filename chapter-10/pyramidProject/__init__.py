b37b1b938734e2488cfc0ab1cd6adbbd3ed34947
Bruce Van Horn <actual@contentfour.com>
2023-09-18 22:25:29 -0500

    replaced pyramid project with giza project

---
+++ chapter-10/pyramidProject/__init__.py
@@ -0,0 +1,12 @@
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    with Configurator(settings=settings) as config:
+        config.include('pyramid_jinja2')
+        config.include('.routes')
+        config.include('.models')
+        config.scan()
+    return config.make_wsgi_app()
